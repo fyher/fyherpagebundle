1bf1549ea9bc5f51e8ac28780dee9ab54a054277
Fyher <contact@fyher.com>
2018-10-01 11:42:31 +0200

    first commit

---
+++ FyherPageBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Fyher\PageBundle;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+class FyherPageBundle extends Bundle
+{
+}
